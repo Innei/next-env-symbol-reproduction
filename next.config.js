df18313b904bc2d4d9ab6d0ac77f7992a4294ec4
Innei <i@innei.in>
2023-12-27 17:03:03 +0800

    add
    Signed-off-by: Innei <i@innei.in>

--- next.config.js
+++ next.config.js
@@ -3,4 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+console.log('RAW_SYMBOL', process.env.RAW_SYMBOL)
+console.log('WITH_SYMBOL', process.env.WITH_SYMBOL)
 module.exports = nextConfig
